02ae7323e301702c762950904fa724a4ecf5b9c6
Keisuke Mori <47830356+kkmory@users.noreply.github.com>
2021-06-18 02:05:28 +0900

    add out dir

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,6 +2,7 @@ module.exports = {
     siteUrl: 'https://kkmory.dev/',
     generateRobotsTxt: true,
     sitemapSize: 7000,
+    outDir: './out',
     robotsTxtOptions: {
         policies: [
             {
